5d423c241e5984c225e1d37e41d6cb8c7955699c
snpsujon <snpsujon93@gmail.com>
2022-08-18 17:02:38 +0600

    s

---
+++ Migrations/20220806055408_live01.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PurityERP.Migrations
+{
+    public partial class live01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
